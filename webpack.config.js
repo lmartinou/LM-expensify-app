0e09c74d60efa729ec32d672e1fdca03550aaabd
Martinou <Loukas.Martinou@bicworld.com>
2020-03-28 09:10:40 +0200

    Setup Production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
 path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js', 
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,                
-                loader: 'babel-loader' //for a single loader and no options (or options in separate file), can use the loader directive                
-            }, {
-                test: /\.s?css$/, //with the ? the s is optional, so the rule works for both .css and .scss
-                use: [ //use allows for multiple loaders and options
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    
+    return {
+        entry: './src/app.js', 
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,                
+                    loader: 'babel-loader' //for a single loader and no options (or options in separate file), can use the loader directive                
+                }, {
+                    test: /\.s?css$/, //with the ? the s is optional, so the rule works for both .css and .scss
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }                        
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css',
+              })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
